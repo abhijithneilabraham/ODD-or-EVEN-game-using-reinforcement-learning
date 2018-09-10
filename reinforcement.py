4ed7dbb1da2f22bab7fe6c69e9400d487ca6c215
abhijithneilabraham <35420019+abhijithneilabraham@users.noreply.github.com>
2018-09-11 00:52:47 +0530

    Update reinforcement.py

--- reinforcement.py
+++ reinforcement.py
@@ -5,3 +5,16 @@ model.add(InputLayer(batch_input_shape=(1, 5)))
 model.add(Dense(10, activation='sigmoid'))
 model.add(Dense(2, activation='linear'))
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+env = gym.make('oddoreven-v0')
+y = 0.95
+eps = 0.5
+decay_factor = 0.999
+r_avg_list = []
+for i in range(num_episodes):
+    s = env.reset()
+    eps *= decay_factor
+    if i % 100 == 0:
+        print("Episode {} of {}".format(i + 1, num_episodes))
+    done = False
+    r_sum = 0
+   
